ec6eb06aa967fc73783314560ffba83b1c4d8881
arya <arya@skuyyyy.local>
2018-10-15 08:15:44 +0700

    Initial Commit

---
+++ aplikasi yang ke 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aplikasi yang ke 2
+//
+//  Created by arya on 15/10/18.
+//  Copyright © 2018 SatyaProject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
